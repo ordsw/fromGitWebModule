63cbe00349c74d7b921b1e29da554b746f8cb388
yangjian <yangjian@3456@126.com>
2020-04-01 17:06:22 +0800

    new servlet

--- servlet/src/com/practice/servlet/ViewStudentsServlet.java
+++ servlet/src/com/practice/servlet/ViewStudentsServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "AddUserServlet")
-public class AddUserServlet extends HttpServlet {
+@WebServlet(name = "ViewStudentsServlet")
+public class ViewStudentsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
